25d564fa1d325e7b4a09ba1b8d7c9c2345b09ff6
Yotam Berkowitz <yotamberk@gmail.com>
2020-04-20 12:35:27 +0300

    Updates

---
+++ precache-manifest.38af9cbfeb1bb7ef48b95e1cfa489a09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812404a3a851ad9671efa2156006259c",
+    "url": "/react-angle-range/index.html"
+  },
+  {
+    "revision": "4ebe9bfec6a6ae8aa4f4",
+    "url": "/react-angle-range/static/css/main.c9f3402c.chunk.css"
+  },
+  {
+    "revision": "3897e6c82bbd5697b675",
+    "url": "/react-angle-range/static/js/2.77440d41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-angle-range/static/js/2.77440d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebe9bfec6a6ae8aa4f4",
+    "url": "/react-angle-range/static/js/main.a6f0e10b.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/react-angle-range/static/js/main.a6f0e10b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac0273cd4143ef16a08",
+    "url": "/react-angle-range/static/js/runtime-main.b9b0b037.js"
+  }
+]);
